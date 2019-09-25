9435dce4c1f3573a22aa4bcc8daa3e2f678c61ba
Nico Linder <nico@nicoviii.net>
2019-09-25 20:46:14 +0200

    add github integration

---
+++ pack.ps1
@@ -0,0 +1,12 @@
+param (
+    [Parameter(Mandatory = $true)][string]$Version
+)
+
+# Set correct version
+$projectFile = "src/GogApi.DotNet/FSharp/GogApi.DotNet.fsproj"
+(Get-Content $projectFile).replace('$version$', $Version) | Set-Content $projectFile
+(Get-Content $projectFile).replace('<!--<Version>', '<Version>') | Set-Content $projectFile
+(Get-Content $projectFile).replace('</Version>-->', '</Version>') | Set-Content $projectFile
+
+# Pack as NugetPackage
+dotnet pack src/GogApi.DotNet/FSharp -c Release -o ../../..
