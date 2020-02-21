012448c1187c6e3069dbd3e46e1ed5f6960710e2
NicoVIII <nico@nicoviii.net>
2020-02-21 18:46:36 +0100

    fix comparison order for refresh

--- src/GogApi.DotNet/FSharp/Authentication.fs
+++ src/GogApi.DotNet/FSharp/Authentication.fs
@@ -5,66 +5,62 @@ open System
 module Authentication =
     let redirectUri = "https://embed.gog.com/on_login_success?origin=client"
 
-    type TokenResponse = {
-        expires_in: int;
-        scope: string;
-        token_type: string;
-        access_token: string;
-        user_id: string;
-        refresh_token: string;
-        session_id: string;
-    }
+    type TokenResponse =
+        { expires_in: int
+          scope: string
+          token_type: string
+          access_token: string
+          user_id: string
+          refresh_token: string
+          session_id: string }
 
     let createAuth response =
         match response with
         | Ok response ->
-            Auth {
-                accessToken = response.access_token
-                refreshToken = response.refresh_token
-                // Safety second to avoid errors through code execution duration
-                accessExpires = response.expires_in - 1 |> float |> DateTimeOffset.UtcNow.AddSeconds
-                }
-        | Error _ ->
-            NoAuth
+            Auth
+                { accessToken = response.access_token
+                  refreshToken = response.refresh_token
+                  // Safety second to avoid errors through code execution duration
+                  accessExpires =
+                      response.expires_in - 1
+                      |> float
+                      |> DateTimeOffset.UtcNow.AddSeconds }
+        | Error _ -> NoAuth
 
-    let private getBasicQueries () =
-        [
-            createQuery "client_id" "46899977096215655";
-            createQuery "client_secret" "9d85c43b1482497dbbce61f6e4aa173a433796eeae2ca8c5f6129f2dc4de46d9";
-        ]
+    let private getBasicQueries() =
+        [ createQuery "client_id" "46899977096215655"
+          createQuery "client_secret" "9d85c43b1482497dbbce61f6e4aa173a433796eeae2ca8c5f6129f2dc4de46d9" ]
 
-    let newToken (code :string) =
+    let newToken (code: string) =
         async {
-            let! result =
-                getBasicQueries ()
-                |> List.append [ createQuery "grant_type" "authorization_code" ]
-                |> List.append [ createQuery "code" code ]
-                |> List.append [ createQuery "redirect_uri" redirectUri ]
-                |> makeRequest<TokenResponse> NoAuth <| "https://auth.gog.com/token"
+            let! result = getBasicQueries()
+                          |> List.append [ createQuery "grant_type" "authorization_code" ]
+                          |> List.append [ createQuery "code" code ]
+                          |> List.append [ createQuery "redirect_uri" redirectUri ]
+                          |> makeRequest<TokenResponse> NoAuth
+                          <| "https://auth.gog.com/token"
             return createAuth result
         }
 
     let refresh authentication =
         async {
-            let! result =
-                getBasicQueries ()
-                |> List.append [ createQuery "grant_type" "refresh_token" ]
-                |> List.append [ createQuery "refresh_token" authentication.refreshToken ]
-                |> makeRequest<TokenResponse> NoAuth <| "https://auth.gog.com/token"
+            let! result = getBasicQueries()
+                          |> List.append [ createQuery "grant_type" "refresh_token" ]
+                          |> List.append [ createQuery "refresh_token" authentication.refreshToken ]
+                          |> makeRequest<TokenResponse> NoAuth
+                          <| "https://auth.gog.com/token"
             return createAuth result
         }
 
     let refreshAuthentication authentication =
         async {
-            return!
-                // Refresh authentication, when old one expired
-                match authentication with
-                | Auth authenticationData
-                    when
-                        DateTimeOffset.UtcNow |> authenticationData.accessExpires.CompareTo >= 0 ->
-                            refresh authenticationData
-                | _ ->
-                    async { return authentication }
+            // Refresh authentication, when old one expired
+            let! authentication = match authentication with
+                                  | Auth authenticationData when authenticationData.accessExpires
+                                                                 |> DateTimeOffset.UtcNow.CompareTo
+                                                                 >= 0 -> refresh authenticationData
+                                  | _ -> async { return authentication }
+            return authentication
         }
 
 
@@ -74,5 +70,4 @@ module Authentication =
 
             // Execute API function
             let! fncResult = apiFnc authentication
-            return (fncResult, authentication)
-        }
+            return (fncResult, authentication) }
