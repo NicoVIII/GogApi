fee7947e72ae9e2efac259692ba82e502e2d687c
NicoVIII <nico@nicoviii.net>
2020-04-05 09:01:59 +0200

    Finetune a bit more stuff

--- docs/loaders/pageloader.fsx
+++ docs/loaders/pageloader.fsx
@@ -15,5 +15,5 @@ let loader (projectRoot: string) (siteContent: SiteContents) =
     siteContent.Add({title = "API reference"; link = "/Reference/apiref.html"})
     siteContent.Add({title = "Home"; link = "https://nicoviii.github.io/GogApi.DotNet"; icon = "fas fa-home"})
     siteContent.Add({title = "GitHub repo"; link = "https://github.com/NicoVIII/GogApi.DotNet"; icon = "fab fa-github"})
-    siteContent.Add({title = "License"; link = "/license.html"; icon = "far fa-file-alt"})
+    siteContent.Add({title = "License"; link = "https://nicoviii.github.io/GogApi.DotNet/license.html"; icon = "far fa-file-alt"})
     siteContent
