14b95e4a98861a90c8d2b300594b1146a38b4c7c
NicoVIII <nico@nicoviii.net>
2020-05-23 19:19:55 +0000

    Fix license link

--- docs/loaders/pageloader.fsx
+++ docs/loaders/pageloader.fsx
@@ -9,5 +9,5 @@ type Shortcut = {
 let loader (projectRoot: string) (siteContent: SiteContents) =
     siteContent.Add({title = "Home"; link = "https://nicoviii.github.io/GogApi.DotNet"; icon = "fas fa-home"})
     siteContent.Add({title = "GitHub repo"; link = "https://github.com/NicoVIII/GogApi.DotNet"; icon = "fab fa-github"})
-    siteContent.Add({title = "License"; link = "/license.html"; icon = "far fa-file-alt"})
+    siteContent.Add({title = "License"; link = "https://github.com/NicoVIII/GogApi.DotNet/blob/master/LICENSE.md"; icon = "far fa-file-alt"})
     siteContent
